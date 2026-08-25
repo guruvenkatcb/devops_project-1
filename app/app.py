247dbe74a2b61ce50e63b291426fea136358c0b2
guruvenkatcb <guruvenkat12007@gmail.com>
2026-08-25 16:26:23 +0000

    feat: add Flask application

---
+++ app/app.py
@@ -0,0 +1,46 @@
+from flask import Flask, jsonify
+import mysql.connector
+import os
+
+app = Flask(__name__)
+
+
+def get_db_connection():
+    return mysql.connector.connect(
+        host=os.getenv("MYSQL_HOST", "localhost"),
+        user=os.getenv("MYSQL_USER", "appuser"),
+        password=os.getenv("MYSQL_PASSWORD", "apppassword"),
+        database=os.getenv("MYSQL_DATABASE", "devopsdb")
+    )
+
+
+@app.route("/")
+def home():
+    return jsonify({
+        "application": "DevOps 2-Tier Application",
+        "status": "running"
+    })
+
+
+@app.route("/health")
+def health():
+    try:
+        connection = get_db_connection()
+        connection.close()
+
+        return jsonify({
+            "status": "healthy",
+            "database": "connected"
+        }), 200
+
+    except Exception as error:
+        return jsonify({
+            "status": "unhealthy",
+            "database": "disconnected"
+        }), 503
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
+
