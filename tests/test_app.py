06e878664863c33b086acd7ab433c8831bcac5f6
guruvenkatcb <guruvenkat12007@gmail.com>
2026-09-08 15:46:21 +0000

    Add .gitignore and test suite

---
+++ tests/test_app.py
@@ -0,0 +1,15 @@
+from app.app import app
+
+
+def test_home():
+    client = app.test_client()
+
+    response = client.get("/")
+
+    assert response.status_code == 200
+
+    data = response.get_json()
+
+    assert data["application"] == "DevOps 2-Tier Application"
+    assert data["status"] == "running"
+    assert data["version"] == "1.1"
